2a4cb6b70ffbe16d848dbd2f3dfe667a329b8180
akashsinha123 <sakash674@gmail.com>
2018-04-27 06:30:29 +0530

    pahe 1 prod test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,9 +19,7 @@ import 'jquery';
 })
 export class AppComponent implements OnInit {
   public name = 'Angular Starter';
-  public tipe = 'assets/img/tipe.png';
   public twitter = 'https://twitter.com/gdi2290';
-  public url = 'https://tipe.io';
   public showDevModule: boolean = environment.showDevModule;
 
   constructor(
@@ -34,10 +32,3 @@ export class AppComponent implements OnInit {
 
 }
 
-/**
- * Please review the https://github.com/AngularClass/angular-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
